d9d5d4c136e65c41e76ef1678eb84c959b60eac7
Lymie <luashley91@gmail.com>
2022-12-06 14:22:15 +0100

    ash added something

--- src1/project/src/MyNode.java
+++ src1/project/src/MyNode.java
@@ -3,6 +3,7 @@ package tree;
 /**
  * This is a node in a binary search tree. The node holds an integer value.
  *
+ * Ashley gör nåt här hahahahahaha wow.
  * @author Torbjorn Ekman
  * @see MyTree
  */
